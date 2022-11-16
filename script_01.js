ea1f2b152907c2375fc08ab92714bb02d5c267bc
Daniela <Daniela-Haeussler@gmx.de>
2022-11-16 23:33:30 +0100

    challenge done!

--- script_01.js
+++ script_01.js
@@ -16,3 +16,43 @@ output(getSentence(["Ich","bin","Max"],"S"));
 output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
 output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,"," habbe","Sie","die"],"Q"));
+
+function getSentence(arr,INTER)
+{
+    const GAP   =" ";
+    const Letter= {"S":".","Q":"?","E":"!"};
+    let str   = "";
+
+for (let i=0; i<arr.length; i ++) 
+{      
+    if (i < arr.length -1) {
+        str += arr[i] + GAP;
+    } else {
+        switch (INTER) 
+        {
+            case "S":
+                str += arr[i]  + Letter["S"];
+                break;
+            case "Q":
+                 str += arr[i]  + Letter["Q"];
+                break;
+            case "E":
+                str += arr[i]  + Letter["E"];
+                break;
+            default:
+            
+                break;
+        }
+    }
+
+}
+
+return str;
+ 
+    }
+
+// Modul: Ausgabe in Konsole : Test
+// output("hi"); 
+function output(outputStr) {
+    console.log(outputStr);
+};
